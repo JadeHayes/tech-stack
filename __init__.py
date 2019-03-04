681b6f15b7523997e5b5c49405759facab30388c
Jade Hayes <Jade.e.Hayes@gmail.com>
2019-03-03 20:34:52 -0800

    * Running locally.

--- __init__.py
+++ __init__.py
@@ -1,15 +1,1 @@
-"""
-The Flask application that handles the API calls from our on-boarding
-react application
-"""
-from flask import Flask
 
-
-from werkzeug.contrib.fixers import ProxyFix
-from .routes.tech_stack import tech_stack_blueprint
-
-app = Flask(__name__)
-app.wsgi_app = ProxyFix(app.wsgi_app)
-app.secret_key = 'abc'
-
-app.register_blueprint(tech_stack_blueprint, url_prefix='/tech-stack')
---
+++ server.py
@@ -0,0 +1,16 @@
+import os
+
+from flask import Flask, render_template
+from werkzeug.contrib.fixers import ProxyFix
+
+app = Flask(__name__)
+app.wsgi_app = ProxyFix(app.wsgi_app)
+app.secret_key = 'abc'
+
+@app.route('/')
+def tech_stack_render():
+    return render_template('tech_stack.html')
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
